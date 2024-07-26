06457afe10f1e4ab1e5f2ea7b26c6259b613e637
Ubuntu <ubuntu@vps-bc87db4d.vps.ovh.net>
2024-07-26 14:30:38 +0000

    mysql 1

--- app.js
+++ app.js
@@ -2,3 +2,27 @@ const express = require('express');
 const mysql = require('mysql');
 const app = express();
 const port = 3000;
+
+// MySQL connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'Habib',
+    password: 'medstartnow',
+    database: 'pilote'
+  });
+  
+  db.connect(err => {
+    if (err) {
+      console.error('Error connecting to MySQL:', err.stack);
+      return;
+    }
+    console.log('Connected to MySQL as id ' + db.threadId);
+  });
+  
+  app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+  });
